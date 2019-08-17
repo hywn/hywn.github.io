0c540c4652191760a317c0c91a48030a01c2b35d
abcjcce <seawee65@live.com>
2019-08-17 17:33:39 -0400

    added spaces

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -1,99 +1,98 @@
-class ScheduleCanvas {
-	constructor(canvasID,
-		{ startHour=8, endHour=18,
-		blockWidth=100, blockHeight=60,
-		marginX=50, marginY=50,
-		background='#fff', foreground='#000',
-		guideOpacity=0.2, lineHeight=14, textPadding=5, font='12px Arial',
-		shortDays=["mo", "tu", "we", "th", "fr"], longDays=["Mon", "Tue", "Wed", "Thu", "Fri"]}={}) {
-		this.startHour = startHour; this.endHour = endHour; this.blockWidth = blockWidth; this.blockHeight = blockHeight; this.marginX = marginX; this.marginY = marginY; this.background = background; this.foreground = foreground; this.guideOpacity = guideOpacity; this.lineHeight = lineHeight; this.textPadding = textPadding; this.font = font; this.shortDays = shortDays; this.longDays = longDays;
-		
-		this.canvas = document.getElementById(canvasID);
-		this.canvas.width = marginX + longDays.length*blockWidth;
-		this.canvas.height = marginY + (endHour - startHour + 1) * blockHeight;
-		
-		this.c = this.canvas.getContext("2d");
-		this.c.globalAlpha = 1;
-		this.c.font = font;
-		this.c.strokeStyle = foreground;
-		this.c.textAlign = "center";
-		this.c.textBaseline = "middle";
-		
-		this.clear();
-	}
-	clear() { this.c.clearRect(0, 0, this.canvas.width, this.canvas.height) }
-	drawSchedule(text) { window.requestAnimationFrame(() => this.drawScheduleCallback(text)); }
-	drawScheduleCallback(text) { //text
-		this.clear();
-		this.drawTimes();
-		this.drawDOWLabels();
-		this.drawLines();
-		for (let chunk of text.split('\n\n')) {
-			let lines = chunk.split("\n");
-			for (let dow of lines[0].toLowerCase().split(/(?=(?:..)*$)/)) // splits mowefrtu into mo, we, fr, tu
-				for (let block of lines.slice(1, lines.length).map(parseBlock)) // takes remaining lines and parses them into blocks
-					if (this.shortDays.indexOf(dow) != -1)
-						this.drawTextRect(block[0],
-							this.marginX + this.shortDays.indexOf(dow)*this.blockWidth,
-							this.marginY + (block[1] - this.startHour)*this.blockHeight,
-							this.blockWidth,
-							(block[2] - block[1])*this.blockHeight);
-		}
-	}
-	drawTextRect(text, x, y, width, height) {
-		this.c.clearRect(x, y, width, height);
-		this.c.strokeRect(x, y, width, height);
-		
-		this.drawText(this.wrapText(text, width - this.textPadding*2 ), x + width/2, y + height/2);
-	}
-	wrapText(text, targetWidth) {
-		var burrito;
-		let lines = "";
-		while ((burrito = this.getWrap(text, targetWidth)).tail.length != 0) {
-			lines += burrito.head + "\n";
-			text = burrito.tail;
-		}
-		lines += burrito.head;
-		return lines;
-	}
-	getWrap(text, targetWidth) {
-		let words = text.split(" ");
-		for (let i=words.length; i>0; i--) { // loops thru string backwards and finds words that fit in box
-			let head = words.slice(0, i).join(' ');
-			if (this.c.measureText(head).width < targetWidth)
-				return { head: head, tail: words.slice(i, words.length).join(' ') }
-		}
-		return { head: '.', tail: '' }
-	}
-	drawText(text, centerX, centerY) {
-		let lines = text.split("\n");
-		centerY -= Math.floor(lines.length/2 + 1) * this.lineHeight + (lines.length%2 - 1) * this.lineHeight/2;
-		for (let line of lines)
-			this.c.fillText(line, centerX, centerY += this.lineHeight);
-	}
-	drawTimes() { // (num, num, num, num, num)
-		for (let hour=this.startHour; hour<=this.endHour; hour++)
-			this.drawTextRect(hour + "", 0, this.marginY + (hour - this.startHour)*this.blockHeight, this.marginX, this.blockHeight);
-	}
-	drawDOWLabels() {
-		for (let day=0; day<this.longDays.length; day++) this.drawTextRect(this.longDays[day], this.marginX + day*this.blockWidth, 0, this.blockWidth, this.marginY );
-	}
-	drawLines(){
-		for (let hour=this.startHour+1; hour<=this.endHour; hour++) {
-			this.c.globalAlpha = this.guideOpacity;
-			this.c.strokeRect(this.marginX, this.marginY + (hour - this.startHour)*this.blockHeight, this.canvas.width - this.marginY, 0);
-			this.c.globalAlpha = 1;
-		}
-	}
-}
+class ScheduleCanvas
+     { constructor(canvasID, { startHour=8, endHour=18,
+                               blockWidth=100, blockHeight=60,
+                               marginX=50, marginY=50,
+                               background='#fff', foreground='#000',
+                               guideOpacity=0.2, lineHeight=14, textPadding=5, font='12px Arial',
+                               shortDays=['mo', 'tu', 'we', 'th', 'fr'], longDays=['Mon', 'Tue', 'Wed', 'Thu', 'Fri']}={})
+            {
+                   { this.startHour = startHour; this.endHour = endHour; this.blockWidth = blockWidth; this.blockHeight = blockHeight;
+                     this.marginX = marginX; this.marginY = marginY; this.background = background; this.foreground = foreground;
+                     this.guideOpacity = guideOpacity; this.lineHeight = lineHeight; this.textPadding = textPadding;
+                     this.font = font; this.shortDays = shortDays; this.longDays = longDays }
+
+              this.canvas = document.getElementById(canvasID)
+              this.canvas.width = marginX + longDays.length*blockWidth
+              this.canvas.height = marginY + (endHour - startHour + 1) * blockHeight
+
+              this.c = this.canvas.getContext('2d')
+              this.c.globalAlpha = 1
+              this.c.font = font
+              this.c.strokeStyle = foreground
+              this.c.textAlign = 'center'
+              this.c.textBaseline = 'middle'
+
+              this.clear() }
+
+       clear()
+            { this.c.clearRect(0, 0, this.canvas.width, this.canvas.height) }
+
+       drawSchedule(text) { window.requestAnimationFrame(() => this.drawScheduleCallback(text)); }
+
+       drawScheduleCallback(text)
+            { this.clear()
+              this.drawTimes()
+              this.drawDOWLabels()
+              this.drawLines()
+              for (let chunk of text.split('\n\n'))
+                   { let lines = chunk.split('\n')
+                     for (let dow of lines[0].toLowerCase().split(/(?=(?:..)*$)/)) // splits mowefrtu into mo, we, fr, tu
+                            for (let block of lines.slice(1, lines.length).map(parseBlock)) // takes remaining lines and parses them into blocks
+                                   if (this.shortDays.indexOf(dow) != -1)
+                                          this.drawTextRect(block[0],
+                                                            this.marginX + this.shortDays.indexOf(dow)*this.blockWidth,
+                                                            this.marginY + (block[1] - this.startHour)*this.blockHeight,
+                                                            this.blockWidth,
+                                                            (block[2] - block[1])*this.blockHeight) } }
+
+       drawTextRect(text, x, y, width, height)
+            { this.c.clearRect(x, y, width, height)
+              this.c.strokeRect(x, y, width, height)
+
+              this.drawText(this.wrapText(text, width - this.textPadding*2 ), x + width/2, y + height/2) }
+
+       wrapText(text, targetWidth)
+            { let burrito
+              let lines = ''
+              while ((burrito = this.getWrap(text, targetWidth)).tail.length != 0)
+                   { lines += burrito.head + '\n'
+                     text = burrito.tail }
+              lines += burrito.head
+              return lines }
+
+       getWrap(text, targetWidth)
+            { let words = text.split(' ');
+              for (let i=words.length; i>0; i--) // loops thru string backwards and finds words that fit in box
+                   { let head = words.slice(0, i).join(' ');
+                     if (this.c.measureText(head).width < targetWidth)
+                            return { head: head, tail: words.slice(i, words.length).join(' ') } }
+              return { head: '.', tail: '' } }
+
+       drawText(text, centerX, centerY)
+            { let lines = text.split('\n')
+              centerY -= Math.floor(lines.length/2 + 1) * this.lineHeight + (lines.length%2 - 1) * this.lineHeight/2
+              for (let line of lines)
+                     this.c.fillText(line, centerX, centerY += this.lineHeight) }
+
+       drawTimes() // (num, num, num, num, num)
+            { for (let hour=this.startHour; hour<=this.endHour; hour++)
+                   this.drawTextRect(hour + '', 0, this.marginY + (hour - this.startHour)*this.blockHeight, this.marginX, this.blockHeight) }
+
+       drawDOWLabels()
+            { for (let day=0; day<this.longDays.length; day++)
+                   this.drawTextRect(this.longDays[day], this.marginX + day*this.blockWidth, 0, this.blockWidth, this.marginY ) }
+
+       drawLines()
+            { for (let hour=this.startHour+1; hour<=this.endHour; hour++)
+                 { this.c.globalAlpha = this.guideOpacity
+                   this.c.strokeRect(this.marginX, this.marginY + (hour - this.startHour)*this.blockHeight, this.canvas.width - this.marginY, 0)
+                   this.c.globalAlpha = 1 } } }
 
 /* parsing */
 
-function parseBlock(string) { // "HH:MM-HH:MM Label"
-	return [string.substr(12), parseHours(string, 0, 5), parseHours(string, 6, 5)];
-}
+function parseBlock(string) // 'HH:MM-HH:MM Label'
+     { return [string.substr(12), parseHours(string, 0, 5), parseHours(string, 6, 5)] }
 
-function parseHours(string, start, end) {
-	parts = string.substr(start, end).split(":");
-	return parseInt(parts[0]) + parseInt(parts[1])/60;
-}
+function parseHours(string, start, end)
+     { parts = string.substr(start, end).split(':')
+       return parseInt(parts[0]) + parseInt(parts[1])/60 }
--- schedule/test.js
+++ schedule/test.js
@@ -1,97 +1,84 @@
-function log(text) {
-	
-	document.getElementById("console-output").value += text + "\n";
-	
-}
-
 function promiseClass(classCode, pfunc) {
-	scurl("https://rabi.phys.virginia.edu/mySIS/CS2/sectiontip.php?ClassNumber=" + classCode, text => {
-		let p = new Parser(text);
+       scurl("https://rabi.phys.virginia.edu/mySIS/CS2/sectiontip.php?ClassNumber=" + classCode, text => {
+              let p = new Parser(text)
 
-		p.deleteUntil('class="InfoClass">');
-		p.deleteUntil('class="InfoClass">');
-		let title = p.deleteUntil('<br');
-		
-		let times = [];
+              p.deleteUntil('class="InfoClass">')
+              p.deleteUntil('class="InfoClass">')
+              let title = p.deleteUntil('<br')
 
-		for (let time of getTimes(p)) {
-			timeparts = time.split(' ');
-			times.push ({
-				dow: timeparts[0],
-				starthour: convert12hr(timeparts[1]),
-				endhour: convert12hr(timeparts[3])
-			});
-		}
-		
-		pfunc({title: title, times: times}); // returns updated schedule
-		
-	});
-}
+              let times = []
 
-//generateSchedule("15450\n11975\n16647\n18452\n19348\n18453\n19350\n16036\n16142");
+              for (let time of getTimes(p)) {
+                     timeparts = time.split(' ')
+
+                     times.push ({ dow: timeparts[0],
+                                   starthour: convert12hr(timeparts[1]),
+                                   endhour: convert12hr(timeparts[3]) }) }
+
+              pfunc({ title: title, times: times }) }) }
 
 function promiseSchedule(classCodes, pfunc) {
-	let append = '';
-	if (classCodes.length == 0) { pfunc(""); return; }
-	for (let classCode of classCodes) {
-		promiseClass(classCode, cls => {
-			for (time of cls.times) {
-				append += `${time.dow}\n${time.starthour}-${time.endhour} ${cls.title}\n\n`;
-			}
-			pfunc(append);
-		});
-	}
+       let append = '';
+       if (classCodes.length == 0) { pfunc(""); return; }
+       for (let classCode of classCodes) {
+              promiseClass(classCode, cls => {
+                     for (time of cls.times) {
+                            append += `${time.dow}\n${time.starthour}-${time.endhour} ${cls.title}\n\n`;
+                     }
+                     pfunc(append);
+              });
+       }
 }
 
 function getTimes(p, times=[]) { // returns something like ["MoWeFr 9:00AM - 9:50AM", "Th 8:30AM - 9:20AM"]
-	if (times.length == 0) p.deleteUntil('class="InfoMeetings"');
-	p.deleteUntil('</td><td>');
-	times.push(p.deleteUntil("</td><td>"));
-	p.continueUntil("</table>")
-	if (p.getBuffer().includes('<strong>')) { p.reset(); return getTimes(p, times); }
-	return times;
+       if (times.length == 0) p.deleteUntil('class="InfoMeetings"');
+       p.deleteUntil('</td><td>');
+       times.push(p.deleteUntil("</td><td>"));
+       p.continueUntil("</table>")
+       if (p.getBuffer().includes('<strong>')) { p.reset(); return getTimes(p, times); }
+       return times;
 }
 
 function convert12hr(textTime) { // 10:00PM -> 22:00
-	let numbers = textTime.match(/\d{1,}/g).map(num => parseInt(num));
-	let ampm = textTime.toLowerCase().match(/am|pm/g)[0] === 'pm' ? 12 : 0;
-	return `${(numbers[0] % 12 + ampm).toString().padStart(2, "0")}:${numbers[1].toString().padStart(2, "0")}`;
+       let numbers = textTime.match(/\d{1,}/g).map(num => parseInt(num));
+       let ampm = textTime.toLowerCase().match(/am|pm/g)[0] === 'pm' ? 12 : 0;
+       return `${(numbers[0] % 12 + ampm).toString().padStart(2, "0")}:${numbers[1].toString().padStart(2, "0")}`;
 }
 
 function scurl(url, cfunc) {
-	return $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', response => cfunc(response.contents));
+       return $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', response => cfunc(response.contents));
 }
 
 class Parser{
-	constructor(text) {
-		this.text = text;
-		this.bindex = 0;
-		this.bsize = 0;
-	}
-	getBuffer() {
-		return this.text.substring(this.bindex, this.bindex + this.bsize);
-	}
-	getBufferRemoveString(string) {
-		return this.text.substring(this.bindex, this.bindex + this.bsize - string.length);
-	}
-	reset() {
-		this.bsize = 0;
-	}
-	clear(string) {
-		let buffer = string? this.getBufferRemoveString(string) : this.getBuffer();
-		this.bindex += this.bsize;
-		this.reset();
-		return buffer;
-	}
-	continueUntil(string, includeString=false) {
-		while(!this.getBuffer().endsWith(string)){
-			this.bsize++;
-		}
-		if (includeString) return this.getBuffer();
-		return this.getBufferRemoveString(string);
-	}
-	deleteUntil(string) {
-		this.continueUntil(string);
-		return this.clear(string);
-	}
+       constructor(text) {
+              this.text = text;
+              this.bindex = 0;
+              this.bsize = 0;
+       }
+       getBuffer() {
+              return this.text.substring(this.bindex, this.bindex + this.bsize);
+       }
+       getBufferRemoveString(string) {
+              return this.text.substring(this.bindex, this.bindex + this.bsize - string.length);
+       }
+       reset() {
+              this.bsize = 0;
+       }
+       clear(string) {
+              let buffer = string? this.getBufferRemoveString(string) : this.getBuffer();
+              this.bindex += this.bsize;
+              this.reset();
+              return buffer;
+       }
+       continueUntil(string, includeString=false) {
+              while(!this.getBuffer().endsWith(string)){
+                     this.bsize++;
+              }
+              if (includeString) return this.getBuffer();
+              return this.getBufferRemoveString(string);
+       }
+       deleteUntil(string) {
+              this.continueUntil(string);
+              return this.clear(string);
+       }
 }
